46b4342989f4c64c6cfe5435b1c077a4ae14c225
ahmed abdelghany <ahmedabdelghany@MacBook-Pro.local>
2025-09-19 20:28:52 +0300

    fix: Add missing @eslint/eslintrc dependency and improve ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,49 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import pluginNext from "@next/eslint-plugin-next";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
 });
 
 const eslintConfig = [
+  js.configs.recommended,
+
+  {
+    ...compat.extends("next/core-web-vitals")[0],
+    files: ["src/**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: ["./tsconfig.json"],
+        tsconfigRootDir: __dirname,
+      },
+    },
+    plugins: {
+      "react": pluginReact,
+      "react-hooks": pluginReactHooks,
+      "@next/next": pluginNext,
+    },
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-img-element": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "@next/next/no-assign-module-variable": "off",
+      "react/no-find-dom-node": "off",
+      "@typescript-eslint/no-useless-constructor": "off",
+    },
+  },
+
   {
     ignores: [
       "node_modules/**",
@@ -23,19 +57,8 @@ const eslintConfig = [
       "public/**",
       "prisma/**",
       "scripts/**",
-    ]
+    ],
   },
-  ...compat.extends("next/core-web-vitals"),
-  {
-    rules: {
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-img-element": "off",
-      "react-hooks/exhaustive-deps": "off",
-      "@next/next/no-assign-module-variable": "off",
-      "react/no-find-dom-node": "off",
-      "@typescript-eslint/no-useless-constructor": "off",
-    },
-  }
 ];
 
 export default eslintConfig;
